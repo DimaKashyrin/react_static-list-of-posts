b9c887f12ece934f9a812f04dd648e8f6fd02c87
DimaKashyrin <kashyrindima@gmail.com>
2019-07-08 00:16:45 +0300

    Updates

---
+++ precache-manifest.5ee5f33d9c2a81a021759c429e9a1902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8487e96231759b50ba4283205f5f8a2a",
+    "url": "/DimaKashyrin/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "0ba5896f3acf485d6472",
+    "url": "/DimaKashyrin/react_static-list-of-posts/static/css/main.196bb266.chunk.css"
+  },
+  {
+    "revision": "488be41c4d57f0f45b7d",
+    "url": "/DimaKashyrin/react_static-list-of-posts/static/js/2.1cad53fc.chunk.js"
+  },
+  {
+    "revision": "0ba5896f3acf485d6472",
+    "url": "/DimaKashyrin/react_static-list-of-posts/static/js/main.c1ed37f5.chunk.js"
+  },
+  {
+    "revision": "b105504ddc0e7ea1da1b",
+    "url": "/DimaKashyrin/react_static-list-of-posts/static/js/runtime~main.2bcc85f1.js"
+  }
+]);
